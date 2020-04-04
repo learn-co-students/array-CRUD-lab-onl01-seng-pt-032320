d171e02baf884d64051e2e97259df22ba47c2e1c
Andre Cunningham <andrecunningham62@gmail.com>
2020-04-04 02:59:05 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,15 +1,18 @@
 
+def create_an_empty_array 
+  []
+end
 
 def create_an_array
- array = ["array"]
+ array = ["array", "andre", "cunningham", "flatiron"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "element"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift ("element")
+  array.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -21,7 +24,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array [0,1]
+  array [index_number]
 end
 
 def retrieve_first_element_from_array(array)
@@ -29,5 +32,5 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array[1]
+  array[-1]
 end
